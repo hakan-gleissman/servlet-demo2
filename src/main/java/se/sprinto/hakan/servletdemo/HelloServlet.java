cd438cec33ad804fe29f71c28efb1b35eee36649
hakan-gleissman <hakan.gleissman@gmail.com>
2025-12-30 23:36:59 +0100

    ölk

--- src/main/java/se/sprinto/hakan/servletdemo/HelloServlet.java
+++ src/main/java/se/sprinto/hakan/servletdemo/HelloServlet.java
@@ -13,6 +13,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.getWriter().println("Hallå nu är texten uppdaterad!");
+        resp.getWriter().println("Wohoo!");
         resp.getWriter().println(req.getParameter("name"));
         String path = req.getPathInfo();
         resp.getWriter().println(path != null && !path.isEmpty() ? path.substring(1) : "");
